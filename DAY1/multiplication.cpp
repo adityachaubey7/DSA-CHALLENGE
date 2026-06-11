161d77d4395c790af6d499fd63dd93a94355186e
Aditya Chaubey <chaubeyaditya252@gmail.com>
2026-06-12 00:25:58 +0530

    initial commit

---
+++ DAY1/multiplication.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter a nu";
+    cin>>number;
+    for(int i=1;i<=10;i++){
+        cout<<number<<"*"<<i<<"="<<number*i<<endl;
+    }
+    
+    return 0;
+}
